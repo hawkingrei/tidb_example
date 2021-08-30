dd28755d35f9ca63c85fdb9ae8d1bcb3389187fd
Weizhen Wang <wangweizhen@pingcap.com>
2021-08-30 16:53:26 +0800

    improve CRUD example for gorm
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- gorm/main.go
+++ gorm/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,6 +14,13 @@ type Order struct {
 	Price    float64 `gorm:"column:price"`
 }
 
+func PrintResult(tx *gorm.DB, result []Order) {
+	if tx.Error == nil && tx.RowsAffected > 0 {
+		for _, order := range result {
+			fmt.Printf("%+v\n", order)
+		}
+	}
+}
 func main() {
 	dsn := "root:@tcp(localhost:4000)/gorm?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -25,7 +34,9 @@ func main() {
 	// Insert
 	db.Create(&Order{Username: "a", Price: 100})
 	db.Create(&Order{Username: "b", Price: 200})
-	db.Create(&Order{Username: "c", Price: 200})
+	db.Create(&Order{Username: "c", Price: 300})
+	db.Create(&Order{Username: "d", Price: 400})
+	db.Create(&Order{Username: "e", Price: 500})
 
 	// Delete
 	db.Delete(&Order{}, 1)
@@ -34,4 +45,18 @@ func main() {
 	// Update
 	db.Model(&Order{}).Where("id = ?", 2).Update("Username", "hello")
 
+	var orders []Order
+	// Get all records
+	result := db.Find(&orders)
+	PrintResult(result, orders)
+
+	// Get records with conditions
+	result = db.Where("username IN ?", []string{"b", "c", "d", "e"}).Find(&orders)
+	PrintResult(result, orders)
+
+	result = db.Where("price >= ?", 300).Find(&orders)
+	PrintResult(result, orders)
+
+	result = db.Raw("SELECT * FROM orders WHERE price = ?", 500).Scan(&orders)
+	PrintResult(result, orders)
 }
